1582380bc35f04d76b8052ec365ce3202b9024bf
Aleksandar <a.plahcinski@gmail.com>
2018-07-19 18:21:49 +0200

    Full options menu, main menu shaped

--- src/aleksandar43/wallbreaker/main/WallBreaker.java
+++ src/aleksandar43/wallbreaker/main/WallBreaker.java
@@ -1,19 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/* */
 package aleksandar43.wallbreaker.main;
 
 import aleksandar43.wallbreaker.gui.MenuText;
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -22,18 +18,80 @@ import javafx.stage.Stage;
  * @author Aleksandar
  */
 public class WallBreaker extends Application {
-    VBox root;
+    private VBox mainMenu, optionsMenu;
+    private boolean musicOn=true, soundEffectsOn=true;
+    private StackPane menusStackPane;
     @Override
     public void start(Stage primaryStage) {
-        root=new VBox();
-        root.setAlignment(Pos.CENTER);
-        root.setStyle("-fx-background-color: black;");
-        root.getChildren().add(new MenuText("Pokreni igru"));
-        root.getChildren().add(new MenuText("Opcije"));
-        root.getChildren().add(new MenuText("Rang lista"));
-        root.getChildren().add(new MenuText("O igri"));
+        optionsMenu=new VBox();
+        optionsMenu.setAlignment(Pos.CENTER);
+        optionsMenu.setStyle("-fx-background-color: black");
+        MenuText musicToggle=new MenuText("Muzika: uklj.");
+        musicToggle.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                if(musicOn){
+                    musicToggle.setText("Muzika: isklj.");
+                    //turn music off
+                } else{
+                    musicToggle.setText("Muzika: uklj.");
+                    //turn music on
+                }
+                musicOn=!musicOn;
+            }
+        });
+        MenuText soundEffectsToggle=new MenuText("Efekti: uklj.");
+        soundEffectsToggle.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                if(soundEffectsOn){
+                    soundEffectsToggle.setText("Efekti: isklj.");
+                    //turn soundEffects off
+                } else{
+                    soundEffectsToggle.setText("Efekti: uklj.");
+                    //turn soundEffects on
+                }
+                soundEffectsOn=!soundEffectsOn;
+            }
+        });
+        MenuText fullscreenToggle=new MenuText("Pređi na ceo ekran");
+        fullscreenToggle.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                if(primaryStage.isFullScreen()){
+                    primaryStage.setFullScreen(false);
+                    fullscreenToggle.setText("Pređi na ceo ekran");
+                } else{
+                    primaryStage.setFullScreen(true);
+                    fullscreenToggle.setText("Pređi na prozor");
+                }
+            }
+        });
+        MenuText backToMain=new MenuText("Nazad");
+        backToMain.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                mainMenu.toFront();
+            }
+        });
+        optionsMenu.getChildren().addAll(musicToggle,soundEffectsToggle,fullscreenToggle,backToMain);
+        
+        mainMenu=new VBox();
+        mainMenu.setAlignment(Pos.CENTER);
+        mainMenu.setStyle("-fx-background-color: black");
+        mainMenu.getChildren().add(new MenuText("Pokreni igru"));
+        MenuText goToOptions = new MenuText("Opcije");
+        goToOptions.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                optionsMenu.toFront();
+            }
+        });
+        mainMenu.getChildren().add(goToOptions);
+        mainMenu.getChildren().add(new MenuText("Rang lista"));
+        mainMenu.getChildren().add(new MenuText("O igri"));
         MenuText qm=new MenuText("Izađi");
-        root.getChildren().add(qm);
+        mainMenu.getChildren().add(qm);
         qm.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
@@ -42,10 +100,14 @@ public class WallBreaker extends Application {
             }
         });
         
-        Scene scene = new Scene(root, 800, 450);
+        menusStackPane = new StackPane();
+        menusStackPane.getChildren().addAll(optionsMenu,mainMenu);
+        Scene scene = new Scene(menusStackPane, 800, 450);
         
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("WallBreaker");
         primaryStage.setScene(scene);
+        primaryStage.setFullScreenExitHint(null);
+        primaryStage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
         primaryStage.show();
     }
 
