e42a1e21dcf2a95dc2bb157abc0e1a51f48e5435
Aleksandar <a.plahcinski@gmail.com>
2018-07-20 23:22:09 +0200

    Start of Brick classes

---
+++ src/aleksandar43/wallbreaker/game/Brick.java
@@ -0,0 +1,23 @@
+/* */
+package aleksandar43.wallbreaker.game;
+
+import javafx.scene.Group;
+
+/**
+ *
+ * @author Aleksandar
+ */
+public class Brick extends Group{
+    protected String type;
+    public Brick(String type){
+        this.type=type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
---
+++ src/aleksandar43/wallbreaker/game/RectangleBrick.java
@@ -0,0 +1,24 @@
+/* */
+package aleksandar43.wallbreaker.game;
+
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
+
+/**
+ *
+ * @author Aleksandar
+ */
+public class RectangleBrick extends Brick{
+    protected Rectangle rectangle;
+    public RectangleBrick(double x, double y, double width, double height, Paint fill){
+        super("rectangle");
+        rectangle=new Rectangle(x, y, width, height);
+        rectangle.setFill(fill);
+        getChildren().add(rectangle);
+    }
+    
+    public RectangleBrick(double x, double y, double width, double height){
+        this(x, y, width, height, Color.BLACK);
+    }
+}
